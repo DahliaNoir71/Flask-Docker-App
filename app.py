10a76b479160497d98e5971514b3a99da3c5cb80
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-11-08 10:56:38 +0100

    WIP

--- app.py
+++ app.py
@@ -8,14 +8,13 @@ app = Flask(__name__)
 # Configuration de la base de données SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Initialiser la base de données avec l'application Flask
 db = SQLAlchemy(app)
+
 
 # Créer la base de données
 with app.app_context():
     db.create_all()
 
-create_routes(app)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
