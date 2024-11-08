0ffa51f21cc2dd009d2e0e3442d7010c67bd5b83
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-11-08 11:17:27 +0100

    Routes : displayers and API CRUD

--- routes/routes.py
+++ routes/routes.py
@@ -1,56 +1,19 @@
 from flask import request, jsonify, render_template
+from db import db
 from models.todo import Todo
-from app import db
 
-def create_display_routes(app):
-    """
-    This function creates routes for displaying a list of todos.
 
-    Parameters:
-    app (Flask): The Flask application instance.
-
-    Returns:
-    None. This function registers routes within the Flask application.
-    """
-
-    @app.route('/', methods=['GET'])
-    @app.route('/todos', methods=['GET'])
+def create_routes(app):
+    # Route pour afficher la liste des todos
+    @app.route('/')
+    @app.route('/todos')
     def get_todos():
-        """
-        This route retrieves all todos from the database and renders them in a template.
-
-        Parameters:
-        None. This function uses the Flask request context.
-
-        Returns:
-        render_template: A rendered HTML template with a list of todos.
-        """
         todos = Todo.query.all()
         return render_template('todos_list.html', todos=todos)
 
-
-def create_crud_routes(app):
-    """
-    This function creates routes for performing CRUD operations on a list of todos.
-
-    Parameters:
-    app (Flask): The Flask application instance.
-
-    Returns:
-    None. This function registers routes within the Flask application.
-    """
-
+    # Route pour ajouter un todo (POST)
     @app.route('/todos', methods=['POST'])
     def add_todo():
-        """
-        This route adds a new todo to the database.
-
-        Parameters:
-        None. This function uses the Flask request context to get JSON data.
-
-        Returns:
-        jsonify: A JSON response containing the added todo and a status code of 201 (Created).
-        """
         data = request.get_json()
         title = data.get('title')
         description = data.get('description', '')
@@ -61,40 +24,27 @@ def create_crud_routes(app):
 
         return jsonify(new_todo.to_dict()), 201
 
+    # Route pour modifier un todo (PUT)
+    @app.route('/todos/<int:id>', methods=['PUT'])
+    def update_todo(id):
+        todo = Todo.query.get(id)
+        if not todo:
+            return jsonify({"error": "Todo not found"}), 404
+
+        data = request.get_json()
+        todo.title = data.get('title', todo.title)
+        todo.description = data.get('description', todo.description)
+
+        db.session.commit()
+        return jsonify(todo.to_dict())
+
+    # Route pour supprimer un todo (DELETE)
     @app.route('/todos/<int:id>', methods=['DELETE'])
     def delete_todo(id):
-        """
-        This route deletes a todo from the database.
-
-        Parameters:
-        id (int): The unique identifier of the todo to delete.
-
-        Returns:
-        jsonify: A JSON response with a success message and a status code of 200 (OK) if the todo is found and deleted.
-                 A JSON response with an error message and a status code of 404 (Not Found) if the todo is not found.
-        """
         todo = Todo.query.get(id)
-        if todo is None:
-            return jsonify({"error": "Task not found"}), 404
+        if not todo:
+            return jsonify({"error": "Todo not found"}), 404
 
         db.session.delete(todo)
         db.session.commit()
-        return jsonify({"message": "Task deleted"}), 200
-
-
-def create_routes(app):
-    """
-    This function creates routes for displaying and performing CRUD operations on a list of todos.
-
-    Parameters:
-    app (Flask): The Flask application instance. This instance is used to register routes within the Flask application.
-
-    Returns:
-    None. This function does not return any value. It registers routes within the Flask application.
-    """
-    create_display_routes(app)
-    create_crud_routes(app)
-
-# Import and call the create_routes function from the app module
-from app import app
-create_routes(app)
+        return jsonify({"message": "Todo deleted"}), 200
