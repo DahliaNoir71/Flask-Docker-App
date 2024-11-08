1af34b8d688998b5d14854cb569cc535fd5dbc63
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-11-08 12:51:26 +0100

    Dockerisation

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, jsonify, render_template
+import os
 from db import db
+
+from flask import Flask
 from routes.routes import create_routes
 
 app = Flask(__name__)
 
-# Configuration de la base de données SQLite
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db_directory = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db')
+if not os.path.exists(db_directory):
+    os.makedirs(db_directory)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(db_directory, "todo.db")}'
+# Désactiver le tracking des modifications pour éviter des warning SQLAlchemy
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialisation de SQLAlchemy
@@ -18,4 +24,4 @@ with app.app_context():
 create_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
